b217f5deefbdfc1e2e9ce60b4c49206bbc311909
Kayla Jones <KaylaLyne424@gmail.com>
2014-07-29 15:02:46 -0500

    learning dynamic stuff

---
+++ app/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/static'));
+app.get('/', function(req,res){
+  res.render('home');
+});
+
+//dynamic port changing
+var port = process.env.PORT;
+app.listen(port, function(){
+  console.log('Express is listening on PORT', port);
+});
