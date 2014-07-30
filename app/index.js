d5f36a5f27bb946930c1c68dba442ca9455efbfc
Kayla Jones <KaylaLyne424@gmail.com>
2014-07-30 17:03:58 -0500

    initial rolldice code, no css

--- app/index.js
+++ app/index.js
@@ -17,6 +17,47 @@ app.get('/checkers', function(req,res){
   res.render('checkers');
 });
 
+app.get('/add/:w/:x/:y/:z', function(req,res){
+  req.params.w *= 1;
+  req.params.x *= 1;
+  req.params.y *= 1;
+  req.params.z *= 1;
+
+  console.log(req.params, req.query);
+  req.params.fontsize = req.query.fontsize;//adds fontsize key/value to the req.param
+  req.params.bordercolor = req.query.bordercolor;
+  req.params.borderwidth = req.query.borderwidth;
+
+  res.render('sum', req.params);
+});
+
+app.get('/sumlist/:list', function(req,res){
+  var nums = req.params.list.split(',');
+  nums = nums.map(function(x){return x * 1;});
+  var sum = 0;
+  for (var i=0; i<nums.length; i++){
+    sum += nums[i];
+  }
+  var obj = {nums:nums, sum:sum, even:req.query.even, odd:req.query.odd};
+  console.log(obj);
+  res.render('sumlist', obj);
+});
+
+app.get('/rolldice/:x', function(req,res){
+  req.params.x *= 1;
+  var rolls = req.params.x;
+  console.log(rolls);
+  var nums= [];
+  var num = 0;
+  for (var i=0; i<rolls; i++){
+    num = Math.floor(Math.random()*6)+1;
+    nums.push(num);
+  }
+  console.log(nums);
+  var obj = {nums:nums};
+  res.render('rolldice', obj);
+});
+
 //dynamic port changing
 var port = process.env.PORT;
 app.listen(port, function(){
